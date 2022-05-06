7da6b977880cca2df76502f564bccde67b4dbcb5
bryan.wu <bryan.wu@ringcentral.com>
2022-05-06 14:30:21 +0800

    111

--- init.sh
+++ init.sh
@@ -8,24 +8,25 @@ export LOCAL_IP=192.168.1.100
 
 #--
 hostnamectl set-hostname ${NODE_NAME}
+sed -i 's/127.0.1.1 c/127.0.1.1 '${NODE_NAME}''
 
 
 
 #--
 
-tee /etc/netplan/00-installer-config.yaml  <<-'EOF'
-network:
-  ethernets:
-    eth0:
-      dhcp4: no
-      dhcp6: no
-      addresses: [${LOCAL_IP}/24]
-      gateway4: 192.168.1.1
-      nameservers:
-              addresses: [114.114.114.114, 8.8.8.8]
-  version: 2
-EOF
-netplan apply
+# tee /etc/netplan/00-installer-config.yaml  <<-'EOF'
+# network:
+#   ethernets:
+#     eth0:
+#       dhcp4: no
+#       dhcp6: no
+#       addresses: ['${LOCAL_IP}'/24]
+#       gateway4: 192.168.1.1
+#       nameservers:
+#               addresses: [114.114.114.114, 8.8.8.8]
+#   version: 2
+# EOF
+# netplan apply
 
 
 
