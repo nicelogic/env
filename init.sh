2050df9cc80c1a4a3ce0d3d75468aaee9b2305fa
bryan.wu <bryan.wu@ringcentral.com>
2022-05-07 14:51:28 +0800

    111

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 
 #!/bin/sh
 
+./ssh-config/ssh-authorized-keys-upload.sh
 #customize hostname + local ip
 # ./customize.sh
-./ssh-config/ssh-authorized-keys-upload.sh
--- ssh-config/ssh-authorized-keys-upload.sh
+++ ssh-config/ssh-authorized-keys-upload.sh
@@ -1,4 +1,5 @@
 
 #!/bin/sh
 
+echo `pwd`
 mv authorized_keys /root/.ssh/authorized_keys 
