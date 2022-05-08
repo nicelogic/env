6d8a636523b3673685687660dc351f993175cb61
nicelogic <358844436@qq.com>
2022-05-08 22:44:32 +0800

    111

--- init.sh
+++ init.sh
@@ -5,11 +5,7 @@ cp -rf sshd/authorized_keys /root/.ssh/authorized_keys
 timedatectl set-timezone Asia/Shanghai
 
 cd config
-res=$(python3 node-name-config.py)
-if [[ res == 1 ]]
-then
-	echo 'please config node name first'
-fi
+python3 node-name-config.py
 python3 ip-config.py
 cd ..
 
