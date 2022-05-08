2d17cfa48a6f184a631e5c31ee4b87d731823b7a
nicelogic <358844436@qq.com>
2022-05-08 22:19:18 +0800

    1

---
+++ config/ip-config.py
@@ -0,0 +1,15 @@
+
+import os
+import yaml
+
+configYml = open(r'ip-config.yml')
+config = yaml.safe_load(configYml)
+localIp= config['local-ip']
+print('local ip: ' + localIp)
+
+if len(localIp) == 0 :
+	print('local ip not config')
+	exit(1)
+
+os.system('./ip-config.sh ' + localIp)
+
--- init.sh
+++ init.sh
@@ -5,8 +5,12 @@ cp -rf sshd/authorized_keys /root/.ssh/authorized_keys
 timedatectl set-timezone Asia/Shanghai
 
 cd config
-python3 node-name-config.py
+res=$(python3 node-name-config.py)
+if res == 1 then
+	echo 'please config node name first'
+fi
 python3 ip-config.py
+cd ..
 
-./base-components.sh
+#./base-components.sh
 ./k8s-env-config.sh
