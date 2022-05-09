2ce9489d80c13b4e2cc5093f1be6e6766f0fbf52
bryan.wu <bryan.wu@ringcentral.com>
2022-05-09 11:12:57 +0800

    222

--- config/config.py
+++ config/config.py
@@ -6,13 +6,12 @@ import sys
 configYml = open(r'config.yml')
 config = yaml.safe_load(configYml)
 nodeName = config['node-name']
-print('node name: ' + nodeName)
 localIp= config['local-ip']
-print('local ip: ' + localIp)
 
-if len(nodeName) == 0 or len(localIp) == 0 :
+if nodeName is None or localIp is None == 0 :
 	sys.exit(1)
 
+print('node name: ' + nodeName)
 os.system('hostnamectl set-hostname ' + nodeName)
 os.system(r"sed -i 's/^127.0.1.1 .*$/127.0.1.1 %s/' /etc/hosts" %(nodeName))
 os.system('./ip-config.sh ' + localIp)
