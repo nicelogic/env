d01b39a1c1c221c5feaf8f8da2c8b80913b33526
bryan.wu <bryan.wu@ringcentral.com>
2022-06-07 12:13:43 +0800

    haproxy config ok

--- config-master-high-availability/master-high-availability.py
+++ config-master-high-availability/master-high-availability.py
@@ -1,16 +1,19 @@
 
-import os
-import yaml
 import sys
+sys.path.append('..')
+from python_util import util
+import yaml
 
 configYml = open(r'../config.yml')
 config = yaml.safe_load(configYml)
 isMasterHighAvailabilityEnable = config['master-high-availability']['enable']
-print('master high availability enable: ' + str(isMasterHighAvailabilityEnable))
+print('master high availability enable: ' +
+      str(isMasterHighAvailabilityEnable))
 if not isMasterHighAvailabilityEnable:
-	print('master high availability disabled')
-	sys.exit(0)
+    print('master high availability disabled')
+    sys.exit(0)
 
+# keepalived
 keepAlivedPriority = config['master-high-availability']['keepalived']['priority']
 keepAlivedVip = config['master-high-availability']['keepalived']['virtual-ipaddress']
 apiServerVip = config['master-high-availability']['keepalived']['check-apiserver']['apiserver-vip']
@@ -20,14 +23,41 @@ print('keep alived vip: ' + keepAlivedVip)
 print('keep alived api server vip: ' + apiServerVip)
 print('keep alived api server dest port: ' + str(apiServerDestPort))
 
+keepAlivedPrioritySearchText = r"^(.*priority).*$"
+keepAlivedPriorityReplaceText = r"\1 " + str(keepAlivedPriority)
+util.replaceText(keepAlivedPrioritySearchText,
+                 keepAlivedPriorityReplaceText, 'keepalived.conf')
+
+keepAlivedVipSearchText = r'''^(.*virtual_ipaddress {.*$)
+(^\s*).*(\s*$)'''
+keepAlivedVipReplaceText = r'''\1
+\g<2>''' + keepAlivedVip + r'''\3'''
+util.replaceText(keepAlivedVipSearchText,
+                 keepAlivedVipReplaceText, 'keepalived.conf')
+
+apiServerVipSearchText = r'''(^export APISERVER_VIP=).+'''
+apiServerVipReplaceText = r'\g<1>' + apiServerVip
+util.replaceText(apiServerVipSearchText,
+                 apiServerVipReplaceText, 'check_apiserver.sh')
+
+apiServerDestPortSearchText = r'''(^export APISERVER_DEST_PORT=)\d+'''
+apiServerDestPortReplaceText = r'\g<1>' + str(apiServerDestPort)
+util.replaceText(apiServerDestPortSearchText,
+                 apiServerDestPortReplaceText, 'check_apiserver.sh')
+
+apiServerDestPort = config['master-high-availability']['keepalived']['check-apiserver']['apiserver-dest-port']
+print('keep alived priority: ' + str(keepAlivedPriority))
+
+# haproxy
+haproxyNodes = config['master-high-availability']['haproxy']['nodes']
+haproxyCfgFileName = 'haproxy.cfg'
+with open(haproxyCfgFileName, 'r') as haproxCfgFile:
+    haproxyCfgData = haproxCfgFile.read()
+for node in reversed(haproxyNodes):
+    if node not in haproxyCfgData:
+        print('haproxy config: ' + node)
+        haproxyNodeSearchText = r'''^(	#server node-x xxx.xxx.x.xxx:xxxx check)'''
+        haproxyNodeReplaceText = r'\g<1>' + '\n	server ' +  node + ' check'
+        util.replaceText(haproxyNodeSearchText, haproxyNodeReplaceText, 'haproxy.cfg')
 
 
-# if nodeName is None 
-# or localIp is None == 0 
-# or :
-# 	sys.exit(1)
-
-# print('node name: ' + nodeName)
-# os.system('hostnamectl set-hostname ' + nodeName)
-# os.system(r"sed -i 's/^127.0.1.1 .*$/127.0.1.1 %s/' /etc/hosts" %(nodeName))
-# os.system('./ip-config.sh ' + localIp)
