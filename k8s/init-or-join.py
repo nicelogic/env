922a280841cd1623e67641e42eabdef9d2757468
nicelogic <358844436@qq.com>
2022-06-18 15:03:58 +0800

    222

--- k8s/init-or-join.py
+++ k8s/init-or-join.py
@@ -1,2 +1,12 @@
 
+import os
+import yaml
 
+configYml = open(r'../config.yml')
+config = yaml.safe_load(configYml)
+isFirstNode = config['is-first-node']
+isMasterHighAvailabilityEnable = config['master-high-availability']['enable']
+print('is first node: ' + str(isFirstNode), + ', master high availability enable: ' + str(isMasterHighAvailabilityEnable))
+
+if isFirstNode:
+	os.system('./init-master.sh')
