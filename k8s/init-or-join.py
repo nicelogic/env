1b53bd5d4dd9ca6f46e24b7f3904615760fddb41
bryan.wu <bryan.wu@ringcentral.com>
2022-06-20 09:25:03 +0800

    111

--- k8s/init-or-join.py
+++ k8s/init-or-join.py
@@ -6,9 +6,12 @@ configYml = open(r'../config.yml')
 config = yaml.safe_load(configYml)
 isFirstNode = config['is-first-node']
 isMasterHighAvailabilityEnable = config['master-high-availability']['enable']
-print('is first node: ' + str(isFirstNode), + ', master high availability enable: ' + str(isMasterHighAvailabilityEnable))
+print('is first node: ' + str(isFirstNode) +
+      '\nmaster high availability enable: ' + str(isMasterHighAvailabilityEnable))
 
 if isFirstNode:
-	os.system('./init-master.sh')
-else isMasterHighAvailabilityEnable:
-	
+    os.system('./init-master.sh')
+elif isMasterHighAvailabilityEnable:
+    os.system('./join-master.sh')
+else:
+    os.system('./join-worker.sh')
