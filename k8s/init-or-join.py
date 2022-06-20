e5d2e1c3d9adc1fb233aecd62196278768db20ca
nicelogic <358844436@qq.com>
2022-06-20 22:04:25 +0800

    333

--- k8s/init-or-join.py
+++ k8s/init-or-join.py
@@ -12,6 +12,7 @@ print('is first node: ' + str(isFirstNode) +
 if isFirstNode:
     os.system('./init-master.sh')
 elif isMasterHighAvailabilityEnable:
-    os.system('./join-master.sh')
+    #os.system('./join-master.sh')
+    print('template not support join master')
 else:
     os.system('./join-worker.sh')
