fb9a3e536173ad92930609f67dc574ac09cd214d
nicelogic <358844436@qq.com>
2022-06-20 21:28:42 +0800

    444

--- config-ip/config.py
+++ config-ip/config.py
@@ -6,7 +6,15 @@ import sys
 configYml = open(r'../config.yml')
 config = yaml.safe_load(configYml)
 localIp= config['local-ip']
+localIpGateway = config['local-ip-gateway']
+networkInterfaceCard = config['network-interface-card']
 if localIp is None:
 	print('local ip is not config')
 	sys.exit(1)
-os.system('./ip-config.sh ' + localIp)
+if networkInterfaceCard is None:
+	print('network interface card is not config')
+	sys.exit(1)
+if localIpGateway is None:
+	print('local ip gateway is not config')
+	sys.exit(1)
+os.system('./ip-config.sh ' + localIp + ' ' + networkInterfaceCard + ' ' + localIpGateway)
--- config-ip/ip-config.sh
+++ config-ip/ip-config.sh
@@ -2,27 +2,31 @@
 #!/bin/sh
 
 export LOCAL_IP=$1
-echo "in shell, local ip: $LOCAL_IP"
+export NETWORK_INTERFACE_CARD=$2
+export LOCAL_IP_GATEWAY=$3
+echo "local ip: $LOCAL_IP"
+echo "network interface card: $NETWORK_INTERFACE_CARD"
+echo "local ip gateway: $LOCAL_IP_GATEWAY"
 
-eth0Info=`ifconfig eth0`
-isEth0Configured=$(echo $eth0Info | grep "eth0: flags")
-if [ "$isEth0Configured" != "" ]; then
-	echo "eth0 has configured"
-else 
-	echo "eth0 not configured"
-  sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="net.ifnames=0 biosdevname=0"/' /etc/default/grub
-  update-grub
-  reboot
-fi
+# eth0Info=`ifconfig eth0`
+# isEth0Configured=$(echo $eth0Info | grep "eth0: flags")
+# if [ "$isEth0Configured" != "" ]; then
+# 	echo "eth0 has configured"
+# else 
+# 	echo "eth0 not configured"
+#   sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="net.ifnames=0 biosdevname=0"/' /etc/default/grub
+#   update-grub
+#   reboot
+# fi
 
 tee /etc/netplan/00-installer-config.yaml  <<EOF
 network:
   ethernets:
-    eth0:
+    $NETWORK_INTERFACE_CARD:
       dhcp4: no
       dhcp6: no
       addresses: [$LOCAL_IP/24]
-      gateway4: 192.168.1.1
+      gateway4: $LOCAL_IP_GATEWAY
       nameservers:
               addresses: [114.114.114.114, 8.8.8.8]
   version: 2
