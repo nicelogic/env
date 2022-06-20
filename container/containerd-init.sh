91c6aac121f40eb4c0d804d2f019b4d5d6569e0c
bryan.wu <bryan.wu@ringcentral.com>
2022-06-20 23:39:09 +0800

    111

--- container/containerd-init.sh
+++ container/containerd-init.sh
@@ -3,6 +3,8 @@
 
 apt install -y containerd
 
+cp -f ./crictl.yaml /etc/crictl.yaml
+
 mkdir /etc/containerd
 containerd config default > /etc/containerd/config.toml
 cp -f ./config.toml /etc/containerd/config.toml
