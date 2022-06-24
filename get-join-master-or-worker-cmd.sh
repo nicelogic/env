93bd9eac7e234f2de4bab9f31edc6b367307c9ab
bryan.wu <bryan.wu@ringcentral.com>
2022-06-24 16:10:36 +0800

    111

--- get-join-master-or-worker-cmd.sh
+++ get-join-master-or-worker-cmd.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 joinCmd=`kubeadm token create --print-join-command`
-echo "join as worker' cmd:\n"
+printf "join as worker' cmd:\n"
 echo $joinCmd
-echo "\n"
-echo "join as master's cmd:\n"
+printf "\n"
+printf "join as master's cmd:\n"
 token=`kubeadm init phase upload-certs --upload-certs`
-token=$token' --control-plane  --certificate-key '$token
+token=${token#*:}
+joinCmd=$joinCmd' --control-plane  --certificate-key '$token
+echo $joinCmd
+printf "\n"
