970c0d5530769be6f5d21e7701144709fe147824
bryan.wu <bryan.wu@ringcentral.com>
2022-06-26 21:55:52 +0800

    111

--- config-master-high-availability/prepare-haproxy-config-file.py
+++ config-master-high-availability/prepare-haproxy-config-file.py
@@ -7,15 +7,11 @@ from python_util import util
 configYml = open(r'../config.yml')
 config = yaml.safe_load(configYml)
 isMasterHighAvailabilityEnable = config['master-high-availability']['enable']
-networkInterfaceCard = config['network-interface-card']
 print('master high availability enable: ' +
       str(isMasterHighAvailabilityEnable))
 if not isMasterHighAvailabilityEnable:
     print('master high availability disabled')
     sys.exit(0)
-if networkInterfaceCard is None:
-    print('network interface card is not config')
-    sys.exit(1)
 
 haproxyCfgFileName = 'config/haproxy.cfg'
 haproxyNodeSearchText = r'''	server node.*check'''
@@ -27,9 +23,13 @@ haproxyNodes = config['master-high-availability']['haproxy']['nodes']
 with open(haproxyCfgFileName, 'r') as haproxCfgFile:
     haproxyCfgData = haproxCfgFile.read()
 for node in reversed(haproxyNodes):
-    if node not in haproxyCfgData:
-        print('haproxy config: ' + node)
+    nodeName = node['name']
+    nodeIp = node['ip']
+    nodePort = str(node['port'])
+    haproxyNodeItem = nodeName + ' ' + nodeIp + ':' + nodePort
+    if nodeName not in haproxyCfgData:
+        print('haproxy config: ' + str(node))
         haproxyNodeSearchText = r'''^(	#server node-x xxx.xxx.x.xxx:xxxx check)'''
-        haproxyNodeReplaceText = r'\g<1>' + '\n	server ' + node + ' check'
+        haproxyNodeReplaceText = r'\g<1>' + '\n	server ' + haproxyNodeItem + ' check'
         util.replaceText(haproxyNodeSearchText,
                          haproxyNodeReplaceText, haproxyCfgFileName)
