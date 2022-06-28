c7e0eb3bce3dd955d668bc4a01b4ef06a192c736
bryan.wu <bryan.wu@ringcentral.com>
2022-06-28 23:55:30 +0800

    11

--- k8s/in-vip-node-ssh-join-master-or-worker.sh
+++ k8s/in-vip-node-ssh-join-master-or-worker.sh
@@ -2,7 +2,7 @@
 
 export alivedMasterNodeIp=$1
 export IS_MASTER=$2
-echo "alivedMasterNodeIp: $VIP"
+echo "alivedMasterNodeIp: $alivedMasterNodeIp"
 echo "is master: $IS_MASTER"
 
 joinCmd=$(ssh -T root@$alivedMasterNodeIp "bash -s" < ../join-master-or-worker.sh $IS_MASTER)
