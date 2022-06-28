0bf3bf84253e083d7fec0aa3c4640ef89418f1d4
bryan.wu <bryan.wu@ringcentral.com>
2022-06-29 01:44:51 +0800

    111

--- delete-node/delete-node.py
+++ delete-node/delete-node.py
@@ -15,35 +15,36 @@ with open('../config.yml', 'r+') as configYml:
           str(isMasterHighAvailabilityEnable))
     if isMasterHighAvailabilityEnable:
         haproxyNodes = config['master-high-availability']['haproxy']['nodes']
-        for node in reversed(haproxyNodes):
-            nodeName = node['name']
-            if nodeName == localNodeName:
-                print('remove local node: ' + nodeName)
-                haproxyNodes.remove(node)
-                break
-        configYml.seek(0)
-        yaml.dump(config, configYml, sort_keys=False)
-        configYml.truncate()
+        if haproxyNodes is not None:
+            for node in reversed(haproxyNodes):
+                nodeName = node['name']
+                if nodeName == localNodeName:
+                    print('remove local node: ' + nodeName)
+                    haproxyNodes.remove(node)
+                    break
+            configYml.seek(0)
+            yaml.dump(config, configYml, sort_keys=False)
+            configYml.truncate()
 
-        os.chdir('../config-master-high-availability')
-        os.system('python3 prepare-haproxy-config-file.py')
-        os.system('cp -f ./config/haproxy.cfg /etc/haproxy/haproxy.cfg')
-        os.chdir('../delete-node')
+            os.chdir('../config-master-high-availability')
+            os.system('python3 prepare-haproxy-config-file.py')
+            os.system('cp -f ./config/haproxy.cfg /etc/haproxy/haproxy.cfg')
+            os.chdir('../delete-node')
 
-        print('update other master node haproxy cfg')
-        haproxyNodes = config['master-high-availability']['haproxy']['nodes']
-        localIp = config['local-ip']
-        for node in reversed(haproxyNodes):
-            nodeIp = node['ip']
-            if nodeIp != localIp:
-                os.system('scp /etc/haproxy/haproxy.cfg root@' +
-                          nodeIp + ':/etc/haproxy/haproxy.cfg')
-                os.system('ssh -T root@' + nodeIp +
-                          ' systemctl restart haproxy')
+            print('update other master node haproxy cfg')
+            haproxyNodes = config['master-high-availability']['haproxy']['nodes']
+            localIp = config['local-ip']
+            for node in reversed(haproxyNodes):
+                nodeIp = node['ip']
+                if nodeIp != localIp:
+                    os.system('scp /etc/haproxy/haproxy.cfg root@' +
+                              nodeIp + ':/etc/haproxy/haproxy.cfg')
+                    os.system('ssh -T root@' + nodeIp +
+                              ' systemctl restart haproxy')
 
-        os.system('systemctl stop keepalived')
-        os.system('apt remove keepalived')
-        os.system('systemctl stop haproxy')
-        os.system('apt remove haproxy')
+            os.system('systemctl stop keepalived')
+            os.system('apt remove keepalived')
+            os.system('systemctl stop haproxy')
+            os.system('apt remove haproxy')
 
     os.system('./delete-node.sh ' + localNodeName)
