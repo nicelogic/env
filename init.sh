6b8e78b8e6eef6702d91412f6011cdab1e3be7e8
bryan.wu <bryan.wu@ringcentral.com>
2022-06-29 21:37:03 +0800

    support log

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 
 #---------------------
 cd config-node-name
-python3 ./config.py
+python3 ./config.py | tee ../log/config-node-name
 if [ $? == 1 ]; then
 	echo 'node name not config'
 	exit 1
@@ -11,49 +11,49 @@ cd ..
 
 #---------------------
 cd 'time'
-./time.sh
+./time.sh | tee ../log/config-time
 cd ..
 
 #---------------------
 cd source
-./source.sh
+./source.sh | tee ../log/config-source
 cd ..
 
 #---------------------
-cd util
-./util.sh
+cd util 
+./util.sh | tee ../log/config-util
 cd ..
 
 #---------------------
-cd container
-./containerd-init.sh
+cd container 
+./containerd-init.sh | tee ../log/config-container
 cd ..
 
 #---------------------
 cd k8s
-./k8s-install.sh
-./k8s-env-config.sh
+./k8s-install.sh | tee ../log/k8s-install
+./k8s-env-config.sh | tee ../log/k8s-env-config
 cd ..
 
 #---------------------
 cd config-master-high-availability
-python3 config-haproxy.py
-python3 config-keepalived.py
+python3 config-haproxy.py | tee ../log/config-haproxy
+python3 config-keepalived.py | tee ../log/config-keepalived
 cd ..
 
 #-------------------
 cd k8s
 
 cd first-master
-python3 init-master-and-pod-network.py
+python3 init-master-and-pod-network.py | tee ../../log/init-master
 cd ..
 
 cd other-master
-python3 join-master-and-update-all-haproxy.py
+python3 join-master-and-update-all-haproxy.py | tee ../../log/other-master
 cd ..
 
 cd worker
-python3 join-worker.py
+python3 join-worker.py | tee ../../join-worker
 cd ..
 
 cd ..
--- ip-and-ssh.sh
+++ ip-and-ssh.sh
@@ -2,12 +2,12 @@
 
 #---------------------
 cd sshd
-./sshd.sh
+./sshd.sh | tee ../log/config-sshd
 cd ..
 
 #---------------------
 cd config-ip
-python3 ./config.py
+python3 ./config.py | tee ../log/config-ip
 if [ $? == 1 ]; then
 	echo 'ip not config'
 	exit 1
